91a4f11dbc42bcbc7a74f53b96c13a05f28eabb5
jbettaque <bettaque@gmail.com>
2025-12-11 20:22:50 +0100

    env var test

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,3 +1,7 @@
-// since there's no dynamic data here, we can prerender
-// it so that it gets served as a static asset in production
-export const prerender = true;
+import { env } from '$env/dynamic/public';
+
+export function load() {
+	return {
+		svelteTestVar: env.PUBLIC_SVELTE_TEST_VAR
+	};
+}
